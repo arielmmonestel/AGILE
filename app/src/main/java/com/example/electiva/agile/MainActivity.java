fa8e779b42f64e426fb2b5be148e53998c30f645
luisrra17 <luisrra17@gmail.com>
2016-03-06 13:52:11 -0600

    Proof of The Bitchness

--- app/src/main/java/com/example/electiva/agile/MainActivity.java
+++ app/src/main/java/com/example/electiva/agile/MainActivity.java
@@ -1,23 +1,70 @@
 package com.example.electiva.agile;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-//holaaaaaaaa prueba 1
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer reproductor;
+    private MediaPlayer opcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button jugar = (Button) findViewById(R.id.jugarbutton);
+        Button ayuda = (Button) findViewById(R.id.ayudabutton);
+
+        reproductor=MediaPlayer.create(this,R.raw.fondo);
+        reproductor.setLooping(true);
+        reproductor.start();
+
         jugar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity( new Intent(MainActivity.this,game.class));
+                opcion = MediaPlayer.create(MainActivity.this, R.raw.opcion);
+                opcion.start();
+                startActivity(new Intent(MainActivity.this, game.class));
             }
         });
+
+        ayuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opcion = MediaPlayer.create(MainActivity.this, R.raw.opcion);
+                opcion.start();
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (reproductor.isPlaying()){
+            reproductor.stop();
+            reproductor.release();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        reproductor.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        reproductor.pause();
     }
 }
